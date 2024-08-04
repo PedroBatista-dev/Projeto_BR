7d36a1a6ef1c5ad84784dfa2175d2529f4201bc3
Pedro Batista <phmbbatista@gmail.com>
2024-08-04 15:13:25 -0300

    Instalando electron

---
+++ projeto-br/app.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const { addBypassChecker } = require('electron-compile');
+
+addBypassChecker((filePath) => { return filePath.indexOf(app.getAppPath()) === -1 && (/.jpg/.test(filePath) || /.ms/.test(filePath) || /.png/.test(filePath)); });
+
+let appWindow
+
+function createWindow() {
+    appWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080
+    });
+    
+    appWindow.loadFile('dist/projeto-br/browser/index.html');
+
+    // Defina a janela para tela cheia
+    appWindow.setFullScreen(true);
+
+    appWindow.on('closed', function () {
+        appWindow = null;
+    });
+}
+
+// Crie um menu vazio
+const menu = Menu.buildFromTemplate([]);
+  
+// Defina o menu como nulo para ocultar os botões padrão
+Menu.setApplicationMenu(menu);
+
+app.whenReady().then(() => {
+    createWindow();
+});
